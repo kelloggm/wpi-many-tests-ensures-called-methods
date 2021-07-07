4361cfab72949aff580918754588ca7f7380c656
Martin Kellogg <kelloggm@cs.washington.edu>
2021-07-06 17:22:47 -0700

    change the test case to a field, because that's all the CF can infer (for now)

--- src/main/java/wpi/many/tests/ensures/called/methods/App.java
+++ src/main/java/wpi/many/tests/ensures/called/methods/App.java
@@ -4,17 +4,19 @@ import org.checkerframework.checker.calledmethods.qual.CalledMethods;
 
 public class App {
 
+    Object obj;
+
     void requiresToString(@CalledMethods({"toString"}) Object obj) { }
 
     // This method is intentionally unannotated. WPI should infer the following annotation,
     // which will allow the test() method to compile: @EnsuresCalledMethods(expr="#1", methods="toString")
-    void ensuresToStringIsCalled(Object obj) {
-        obj.toString();
+    void ensuresToStringIsCalled() {
+        this.obj.toString();
     }
 
     void test() {
-        Object obj = new Object();
-        ensuresToStringIsCalled(obj);
+        obj = new Object();
+        ensuresToStringIsCalled();
         requiresToString(obj);
     }
 }
