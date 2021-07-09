3c95cf95ca62fcca2c8ce4d0526a9aa985e1b0bf
Martin Kellogg <kelloggm@cs.washington.edu>
2021-07-08 17:20:14 -0700

    add tests for ecm on this and parameters

--- src/main/java/wpi/many/tests/ensures/called/methods/App.java
+++ src/main/java/wpi/many/tests/ensures/called/methods/App.java
@@ -8,15 +8,77 @@ public class App {
 
     void requiresToString(@CalledMethods({"toString"}) Object obj) { }
 
-    // This method is intentionally unannotated. WPI should infer the following annotation,
-    // which will allow the test() method to compile: @EnsuresCalledMethods(expr="this.obj", methods="toString")
+    void requiresToStringAndHashCode(@CalledMethods({"toString", "hashCode"}) Object obj) { }
+
+    // These methods are intentionally unannotated. WPI should infer @EnsuresCalledMethods annotation,
+    // which will allow the test_*() methods to compile.
     void ensuresToStringIsCalled() {
         this.obj.toString();
     }
 
-    void test() {
+    void ensuresToStringIsCalledOnParam(Object obj1) {
+        obj1.toString();
+    }
+
+    void ensuresToStringIsCalledOnThis() {
+        this.toString();
+    }
+
+    void ensuresToStringIsCalledOnParams(Object obj1, Object obj2) {
+        obj1.toString();
+        obj2.toString();
+    }
+
+    void ensuresToStringAndHashCodeIsCalledOnParam(Object obj1) {
+        obj1.toString();
+        obj1.hashCode();
+    }
+
+    // test methods
+
+    void test_field_1() {
         obj = new Object();
         ensuresToStringIsCalled();
         requiresToString(obj);
+    }
+
+    void test_param_1() {
+        Object obj1 = new Object();
+        ensuresToStringIsCalledOnParam(obj1);
+        requiresToString(obj1);
+    }
+
+    void test_param_2() {
+        Object obj1 = new Object();
+        Object obj2 = new Object();
+        ensuresToStringIsCalledOnParam(obj1);
+        ensuresToStringIsCalledOnParam(obj2);
+        requiresToString(obj1);
+        requiresToString(obj2);
+    }
+
+    void test_param_3() {
+        Object obj1 = new Object();
+        Object obj2 = new Object();
+        ensuresToStringIsCalledOnParams(obj1, obj2);
+        requiresToString(obj1);
+        requiresToString(obj2);
+    }
+
+    void test_param_4() {
+        Object obj1 = new Object();
+        ensuresToStringAndHashCodeIsCalledOnParam(obj1);
+        requiresToString(obj1);
+    }
+
+    void test_param_5() {
+        Object obj1 = new Object();
+        ensuresToStringAndHashCodeIsCalledOnParam(obj1);
+        requiresToStringAndHashCode(obj1);
+    }
+
+    void test_this() {
+        ensuresToStringIsCalledOnThis();
+        requiresToString(this);
     }
 }
